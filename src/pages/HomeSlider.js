6c847bb71bc0545818fe5d45682eecbefd393231
David Gomez <dagove47@gmail.comm>
2021-09-07 03:02:33 -0600

    slider changed for react-bootstrap carousel

--- src/pages/HomeSlider.js
+++ src/pages/HomeSlider.js
@@ -4,20 +4,22 @@ import "../styles/HomeSlider.css";
 import Carousel from "react-bootstrap/Carousel";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-// import gif1 from "../assets/HomeSlider1.gif";
-// import gif2 from "../assets/HomeSlider2.gif";
-// import gif3 from "../assets/HomeSlider3.gif";
-// import gif4 from "../assets/HomeSlider4.gif";
-// import gif5 from "../assets/HomeSlider5.gif";
+import gif1 from "../assets/HomeSlider1.gif";
+import gif2 from "../assets/HomeSlider2.gif";
+import gif3 from "../assets/HomeSlider3.gif";
+import gif4 from "../assets/HomeSlider4.gif";
+import gif5 from "../assets/HomeSlider5.gif";
+import gif6 from "../assets/HomeSlider6.gif";
+import gif7 from "../assets/HomeSlider7.gif";
 
 
 export const HomeSlider = () => {
     return (
         <Carousel>
-            <Carousel.Item interval={1400}>
+            <Carousel.Item interval={9000}>
                 <img
                     className="d-block homeSliderContainer"
-                    src="https://images.pexels.com/photos/2902536/pexels-photo-2902536.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+                    src={gif1}
                     alt="First slide"
                 />
                 <Carousel.Caption>
@@ -25,10 +27,10 @@ export const HomeSlider = () => {
                     <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                 </Carousel.Caption>
             </Carousel.Item>
-            <Carousel.Item interval={1400}>
+            <Carousel.Item interval={9000}>
                 <img
                     className="d-block homeSliderContainer"
-                    src="https://images.pexels.com/photos/3837494/pexels-photo-3837494.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+                    src={gif2}
                     alt="Second slide"
                 />
                 <Carousel.Caption>
@@ -36,10 +38,10 @@ export const HomeSlider = () => {
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 </Carousel.Caption>
             </Carousel.Item>
-            <Carousel.Item interval={1400}>
+            <Carousel.Item interval={9000}>
                 <img
                     className="d-block homeSliderContainer"
-                    src="https://images.pexels.com/photos/3690005/pexels-photo-3690005.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+                    src={gif3}
                     alt="Third slide"
                 />
                 <Carousel.Caption>
@@ -47,10 +49,10 @@ export const HomeSlider = () => {
                     <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
                 </Carousel.Caption>
             </Carousel.Item>
-            <Carousel.Item interval={1400}>
+            <Carousel.Item interval={9000}>
                 <img
                     className="d-block homeSliderContainer"
-                    src="https://images.pexels.com/photos/2902541/pexels-photo-2902541.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+                    src={gif4}
                     alt="Fourth slide"
                 />
                 <Carousel.Caption>
@@ -58,15 +60,37 @@ export const HomeSlider = () => {
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 </Carousel.Caption>
             </Carousel.Item>
-            <Carousel.Item interval={1400}>
+            <Carousel.Item interval={9000}>
                 <img
                     className="d-block homeSliderContainer"
-                    src="https://images.pexels.com/photos/8294620/pexels-photo-8294620.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+                    src={gif5}
                     alt="Fifth slide"
                 />
                 <Carousel.Caption>
                     <h3>Fifth slide label</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item interval={9000}>
+                <img
+                    className="d-block homeSliderContainer"
+                    src={gif6}
+                    alt="Sixth slide"
+                />
+                <Carousel.Caption>
+                    <h3>Sixth slide label</h3>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item interval={9000}>
+                <img
+                    className="d-block homeSliderContainer"
+                    src={gif7}
+                    alt="Seventh slide"
+                />
+                <Carousel.Caption>
+                    <h3>Seventh slide label</h3>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
                 </Carousel.Caption>
             </Carousel.Item>
         </Carousel>
