b845d1ff59fbf2633cb2c497febf8eeb37852339
David Gomez <dagove47@gmail.comm>
2021-09-07 03:29:20 -0600

    slider changed for react-bootstrap carousel 4

--- src/pages/HomeSlider.js
+++ src/pages/HomeSlider.js
@@ -6,11 +6,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import gif1 from "../assets/HomeSlider1.gif";
 import gif2 from "../assets/HomeSlider2.gif";
-import gif3 from "../assets/HomeSlider3.gif";
-import gif4 from "../assets/HomeSlider4.gif";
 import gif5 from "../assets/HomeSlider5.gif";
 import gif6 from "../assets/HomeSlider6.gif";
-import gif7 from "../assets/HomeSlider7.gif";
 
 
 export const HomeSlider = () => {
@@ -38,28 +35,7 @@ export const HomeSlider = () => {
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 </Carousel.Caption>
             </Carousel.Item>
-            <Carousel.Item interval={9000}>
-                <img
-                    className="d-block homeSliderContainer"
-                    src={gif3}
-                    alt="Third slide"
-                />
-                <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item interval={9000}>
-                <img
-                    className="d-block homeSliderContainer"
-                    src={gif4}
-                    alt="Fourth slide"
-                />
-                <Carousel.Caption>
-                    <h3>Fourth slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
+            
             <Carousel.Item interval={9000}>
                 <img
                     className="d-block homeSliderContainer"
@@ -79,17 +55,6 @@ export const HomeSlider = () => {
                 />
                 <Carousel.Caption>
                     <h3>Sixth slide label</h3>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item interval={9000}>
-                <img
-                    className="d-block homeSliderContainer"
-                    src={gif7}
-                    alt="Seventh slide"
-                />
-                <Carousel.Caption>
-                    <h3>Seventh slide label test</h3>
                     <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
                 </Carousel.Caption>
             </Carousel.Item>
